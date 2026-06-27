6388aa78ad79807a8a6dd172e028a58f456ad06f
muleiesther8 <muleiesther84@gmail.com>
2026-06-27 19:45:36 +0300

    Security fix: moved SMTP credentials to .env file

--- Week7/includes/mailer.php
+++ Week7/includes/mailer.php
@@ -19,13 +19,15 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // EMAIL CONFIGURATION
 // Update these with your Gmail credentials
 // ============================================================
+$env = parse_ini_file(__DIR__ . '/../.env');
+
 define('MAIL_HOST',     'smtp.gmail.com');
 define('MAIL_PORT',     587);
-define('MAIL_USERNAME', 'muleiesther84@gmail.com');   // ← Your Gmail
-define('MAIL_PASSWORD', 'uuwf elhr icnn ghme');       // ← Gmail App Password
-define('MAIL_FROM',     'muleiesther84@gmail.com');
+define('MAIL_USERNAME', $env['MAIL_USERNAME']);
+define('MAIL_PASSWORD', $env['MAIL_PASSWORD']);
+define('MAIL_FROM',     $env['MAIL_FROM']);
 define('MAIL_FROM_NAME','Essiz Beauty Hub');
-define('SITE_URL', 'https://unaired-myself-mayflower.ngrok-free.dev/EssizBeautyHub/Week7');
+define('SITE_URL',      'https://unaired-myself-mayflower.ngrok-free.dev/EssizBeautyHub/Week7');
 
 // ============================================================
 // SEND EMAIL FUNCTION
